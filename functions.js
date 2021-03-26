6156b24a33ec745c4d7fca48ce6d64c00c4a1036
Maelstrom87 <78507029+Maelstrom87@users.noreply.github.com>
2021-03-26 10:29:31 +0100

    core completed

--- functions.js
+++ functions.js
@@ -102,7 +102,7 @@ function oppositePairing(array1,array2){
         matches.push([array1[i],array2[array2.length-i-1]]);
     }
     console.log(matches);
-    return matches;
+    return matches,turn;
 };
 
 function randomPairing(array1,array2){
@@ -116,18 +116,30 @@ function randomPairing(array1,array2){
         matches.push(match);
     }
     console.log(matches);
-    return matches;
+    return matches,turn;
 };
 
-function printPairing(array){
-    const newLine = document.createElement('li');
- 
+function printPairing(array,turn){
+    //matchWrapper.remove();
+    //newWrapper.setAttribute('id',`turno${turn}`)
+    /*let wrapOnMe;
+    if(!turn){
+        appOnMe = document.querySelector('list-wrapper')
+    }else{
+        appOnMe=
+    */
+
+
+    
+    listWrapper.innerHTML='';
+    matchWrapper.innerHTML='';
     for (var a=0; a<array.length; a++){
         const newPair = document.createElement('ol');
         const matchNumber = document.createElement('li');
         matchNumber.classList.add('match-number')
         matchNumber.appendChild(newPair);
-        listWrapper.appendChild(matchNumber);
+        matchWrapper.appendChild(matchNumber);
+        listWrapper.appendChild(matchWrapper);
         for (var b=0; b<2; b++){
             const newLine = document.createElement('li');
             newLine.innerText = array[a][b].nome;
@@ -167,12 +179,12 @@ function resultTab(line,nome,player,a){
 function resultsCheck (pairArray){
     let resultsArray = [];
     let error=[];
-    let w ;
+    //let w ;
     for (let m = 0; m < pairArray.length ; m++){
         resultsArray.push([]);
         for (let g = 0 ;  g <2; g++ ){
             let iddi = pairArray[m][g].nome;
-            w = document.querySelector(`#${iddi}`);
+            let w = document.querySelector(`#${iddi}`);
             resultsArray[m].push(w.value); //errore nel secondo loop
         }
     }
@@ -186,36 +198,170 @@ function resultsCheck (pairArray){
             const g2 =pairArray[n][1]
         if(p1 + p2 < 4 ){
             if (p1>p2){
-                p1pointer.setAttribute('id','is-winner');
-                p2pointer.setAttribute('id','is-loser');
+                p1pointer.classList.add('is-winner');
+                p2pointer.classList.add('is-loser');
                 g1.risultati.push([p1,p2,'w'])
                 g2.risultati.push([p2,p1,'l'])
+                g1.punteggio += 3;
             }else if(p1<p2){
-                p1pointer.setAttribute('id','is-loser');
-                p2pointer.setAttribute('id','is-winner');
+                p2pointer.classList.add('is-winner');
+                p1pointer.classList.add('is-loser');
                 g1.risultati.push([p1,p2,'l'])
                 g2.risultati.push([p2,p1,'w'])
+                g2.punteggio += 3
             }else {
-                p1pointer.setAttribute('id','is-draw');
-                p2pointer.setAttribute('id','is-draw');
-                g1.push({risultati:[p1,p2,'d']})
-                g2.push({risultati:[p2,p1,'d']})
+                p1pointer.classList.add('is-draw');
+                p2pointer.classList.add('is-draw');
+                g1.risultati.push([p1,p2,'d'])
+                g2.risultati.push([p2,p1,'d'])
+                g1.punteggio += 1;
+                g2.punteggio += 1;
             }
         }else{
-            p1pointer.setAttribute('id','is-error');
-            p2pointer.setAttribute('id','is-error');
+            p1pointer.classList.add('is-error');
+            p2pointer.classList.add('is-error');
             if(!error){
                 
                 error.push(n+1);
             } else {error.push(n+1);}
         }
+        g1.avversari.push(g2);
+        g2.avversari.push(g1);
 
     }
     if(!error.length>0){
         newMatch.removeAttribute('disabled');
     }else{
-        for(let e of error)
+        for(let e of error){
         alert(`errore di registrazione nel match ${e}` )
-    }
+        }
+        for (let m = 0; m < pairArray.length ; m++){
+            for (let g = 0 ;  g <2; g++ ){
+                let iddi = pairArray[m][g].nome;
+                let w = document.querySelector(`#${iddi}`);
+                w.removeAttribute('class');
+
+            }
+        }
+    return resultsArray;
+    };
 };
+const createScoreTable = (array) =>{
+    classList.innerHTML='';
+    let scoreboardTable = document.createElement('table');
+    scoreboardTable.classList.add('scoreboardtable');
+    let scoreboardHeader = document.createElement('thead');
+    scoreboardHeader.classList.add('scoreboardheader');
+    let scoreboardHeaderRow = document.createElement('tr');
+    scoreboardHeaderRow.classList.add('scoreboardheaderrow');
+    tableHeaders.forEach(header =>{
+        let scoreHeader = document.createElement('th');
+        scoreHeader.innerText = header;
+        scoreboardHeaderRow.appendChild(scoreHeader);
+    })
+    scoreboardHeader.appendChild(scoreboardHeaderRow);
+    scoreboardTable.appendChild(scoreboardHeader);
+    let scoreboardBody = document.createElement('tbody')
+    scoreboardBody.classList.add('scoreboardbody')
+    array.forEach(player =>{
+        let playerRow = document.createElement('tr');
+        let playerName= document.createElement('td')
+        playerName.innerText = player.nome;
+        let playerScore = document.createElement('td');
+        playerScore.innerText = player.punteggio;
+        let playerRating = document.createElement('td');
+        playerRating.innerText = player.rating();
+        playerRow.appendChild(playerName);
+        playerRow.appendChild(playerScore);
+        playerRow.appendChild(playerRating);
+        scoreboardBody.appendChild(playerRow);
+        scoreboardTable.appendChild(scoreboardBody);
+
+        classList.appendChild(scoreboardTable);
+
+    })
+    
+
+
+}
+
+
+function createStandings(array){
+ /*   function compare (a,b){
+        return b.punteggio - a.punteggio;}
+    
+    array.sort(compare);*/
+    array.sort(
+        function(a, b) {          
+           if (a.punteggio === b.punteggio) {
+             
+              return b.rating() - a.rating();
+           }
+           return b.punteggio > a.punteggio ? 1 : -1;
+           
+        });
+    createScoreTable(array)
+    
+    /*const chart = document.createElement('ol');
+    for (var a=0; a<array.length; a++){
+        
+        const rank = document.createElement('li');
+        classList.appendChild(chart);
+        rank.classList.add(`rank${a+1}`)
+        rank.innerText =`${array[a].nome}     ${array[a].punteggio}`
+        chart.appendChild(rank);
+        
+    }*/
+        
+
+
+};
+
+function matchRoutine(){
+    turn ++
+    matches= [];
+    allPlayers.forEach((player=>{
+        player.paired = false;
+    }))
+    allPlayers.forEach((player,index)=>{
+        let oppoTry = index +1;
+        if (oppoTry< allPlayers.length){
+            while(player.paired==false){
+            
+                if(!(player.avversari.includes(allPlayers[oppoTry]))){
+                    matches.push([player,allPlayers[oppoTry]])
+                    console.log(matches);
+                    player.paired=true;
+                    allPlayers[oppoTry].paired=true;
+                }else{
+                    oppoTry ++;
+                }
+            }   
+        }else{
+            oppoTry = 0;
+        }
+        
+        
+    })
+    
+    printPairing(matches,turn)
+}
+
+function createPage(turn){
+    let page;
+    page.innerHTML=`
+        <div class="matches turno${turn} page">
+        <div id="list-wrapper">
+        <ol id ="match-list">
+
+        
+        </ol>
+        </div>  
+        <input type="submit" id="confirm" value="conferma">
+        <input type="submit" id="next-match-pc" value="Prossimo Turno">
+    
+    `
+    body.appendChild(page)
+};
+
 
--- mtgpairings.js
+++ mtgpairings.js
@@ -12,32 +12,52 @@ const tablePage = document.querySelector('.table');
 const turnsPage =document.querySelector('.turns');
 const oppo =document.querySelector('#oppo');
 const random = document.querySelector('#random');
-const matches=[];
+let matches=[];
 let firstHalf =[];
 let secondHalf=[];
 let allPlayers=[];
-
 let playersName =[];
 let td ;
 let counter = 0;
 let listWrapper = document.querySelector('#list-wrapper');
+let matchWrapper = document.querySelector('#match-list');
 const newMatch = document.querySelector('#next-match');
-
 const confirm = document.querySelector('#confirm');
+const classificaP = document.querySelector('.classifica');
+const classList = document.querySelector('#class-wrapper');
+const tableHeaders = ['Nome Giocatore','Punteggio','Rating'];
+const nextMatch = document.querySelector('#next-match-pc');
+const matchesPage = document.querySelector('.matches');
+let newWrapper = document.createElement('ol');
+let turn=1;
 
 
 
 class Player {
   constructor(nome) {
     
-    this.nome = nome;
-    this.punteggio = 0;
-    this.avversari = [];
-    this.risultati = [];
-
+  this.nome = nome;
+  this.punteggio = 0;
+  this.avversari = [];
+  this.risultati = [];
+  this.paired = false;
+  this.bufferRis;
+  this.bufferAvv;
+    
+  }
+  calcRating() {
+    let rate = 0;
+    this.avversari.forEach(function(element){
+      rate += element.punteggio;
+    });
+    return rate
+  }
+  get rating (){
+    return this.calcRating
   }
 
-
+    
+  
 }
 let players = [];
 invia.addEventListener('click', () =>{
@@ -93,6 +113,24 @@ confirm.addEventListener('click',(e)=>{
   
   resultsCheck(matches);
 });
+newMatch.addEventListener('click',(e)=>{
+  e.preventDefault();
+  turnsPage.classList.remove('active');
+  classificaP.classList.add('active');
+  createStandings(allPlayers);
+
+})
+
+nextMatch.addEventListener('click', (e)=>{
+  e.preventDefault();
+  classificaP.classList.remove('active');
+  turnsPage.classList.add('active');
+  matchRoutine();
+
+
+}
+
+)
 
 
 
